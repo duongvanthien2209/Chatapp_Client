6ac6b150e78c36e283e09134902fac606bf1735b
Duong Van Thien <duongvanthienbkhoa@gmail.com>
2020-10-11 16:53:52 +0700

    finish fix-1

---
+++ build/precache-manifest.d5e6c226be8f7b7ee8a2552da2fd8e8a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5acc5fb8836b3ccb8c977a1e5595a4b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "891b8e7aad08a48cbd2a",
+    "url": "/static/css/2.6b5d40a7.chunk.css"
+  },
+  {
+    "revision": "d176858c2bc373594884",
+    "url": "/static/css/main.af226235.chunk.css"
+  },
+  {
+    "revision": "891b8e7aad08a48cbd2a",
+    "url": "/static/js/2.ba088ae4.chunk.js"
+  },
+  {
+    "revision": "048fb6323b4c335f27fb3ec904698273",
+    "url": "/static/js/2.ba088ae4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d176858c2bc373594884",
+    "url": "/static/js/main.2e439e2b.chunk.js"
+  },
+  {
+    "revision": "ee30ccd1a12ebc0fa9c0",
+    "url": "/static/js/runtime-main.59ecef11.js"
+  },
+  {
+    "revision": "2fd76396439faaee521e4fcf3f75afed",
+    "url": "/static/media/camp.2fd76396.png"
+  },
+  {
+    "revision": "6eadf5d60b1c0eb5b49ca8123037bca1",
+    "url": "/static/media/person.6eadf5d6.svg"
+  }
+]);
