1e39b37f9911e59a726ed0ba5f9b45513e3f4c20
Duong Van Thien <duongthien2209@gmail.com>
2020-11-18 09:52:10 +0700

    change handleToast

--- src/helpers/handleToast.js
+++ src/helpers/handleToast.js
@@ -1,23 +1,21 @@
+const defaultObj = {
+  position: 'top-right',
+  autoClose: 5000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+};
+
 const handleToast = (toast, message, status = true) => {
   if (status) {
     return toast.success(message, {
-      position: 'top-right',
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
+      ...defaultObj,
     });
   }
   return toast.error(message, {
-    position: 'top-right',
-    autoClose: 5000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
+    ...defaultObj,
   });
 };
 
